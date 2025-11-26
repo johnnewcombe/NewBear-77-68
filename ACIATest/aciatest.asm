fcaeca96f80efc04ed2ea96b12344b18ac675095
John Newcombe (GlassTTY) <jnewcombeuk@gmail.com>
2025-11-26 23:40:37 +0000

    Corrected ORG Statements.

--- ACIATest/aciatest.asm
+++ ACIATest/aciatest.asm
@@ -5,13 +5,15 @@ CTRLB   EQU     $F403   ; ACIA.B Ctrl/Status
 DATAA   EQU     $F400   ; ACIA.A Data register
 DATAB   EQU     $F402   ; ACIA.B Data register
 
-        ORG     $F000
+        ORG     $FF00
 
-        LDAA    #'@'    ; load A with a char
+START   LDAA    #'@'    ; load A with a char
 LOOP    LDAB    CTRLA   ; get ACIA status
         BITB    #2      ; wait for TX buffer empty
         BEQ     LOOP
         STAA    DATAA   ; send char to port
         BRA     LOOP    ; repeat
 
+        ORG     $FFFE
+        FDB     START
         END
